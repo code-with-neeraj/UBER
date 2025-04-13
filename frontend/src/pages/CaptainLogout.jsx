920d2359a890e491f9747a8cb6e43bddbe235432
neerajBro44 <neerajkr145518@gmail.com>
2025-04-13 22:54:49 +0530

    add captainlogout

---
+++ frontend/src/pages/CaptainLogout.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import axios from 'axios'
+import useNavigate from 'react-router-dom'
+
+const CaptainLogout = () => {
+    const token = localStorage.getItem('captain-token')
+    const navigate = useNavigate()
+
+    axios.get(`${import.meta.env.VITE_API_URL}/captains/logout`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then((response) => {
+        if (response.status === 200){
+            localStorage.removeItem('captain-token')
+            navigate('/captain-login')
+        }
+    })
+
+
+return (
+    <div style={{ fontSize: 'large', color: 'red' }}>
+        CaptainLogout
+    </div>
+)
+}
+
+export default CaptainLogout
